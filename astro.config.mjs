ff890c5f602bad631e67c24aaad65e07bed92a62
electricmidnight <lance@luminousstory.com>
2026-05-17 21:02:34 -0600

    Simplify sitemap config to fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://thestoryspeaks.com',
-  integrations: [
-    sitemap({
-      changefreq: 'monthly',
-      priority: 0.7,
-      lastmod: new Date(),
-    }),
-  ],
+  integrations: [sitemap()],
 });
